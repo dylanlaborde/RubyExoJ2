184c12f408b42ddc7aa3a062ba65c127eff8181b
dylanlaborde <dylan4047@gmail.com>
2021-04-13 23:37:20 +0200

    factorisation + ajout option recommencer

--- exo_17.rb
+++ exo_17.rb
@@ -7,21 +7,59 @@
 #########
 =end
 
-puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 
+puts "> Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ? (entre 1 et 25)"
 user_number = gets.chomp.to_i
-
-    if user_number <= 25 && user_number >= 1 
-        user_number.times do |i|
-            
-            ligne_pyra = "#" * (i *2 +1)
-            space = user_number - i
-            puts " " * space + ligne_pyra
-          
-        end
-    else
-        puts "un nombre entre 1 et 25 stp"
+#on recupere l'entrer utilisateur 
+#et on effectuer des verification
+    def Getinput(a)
+        condition(a)
     end
+#fonction de verification
+    def condition(b)
+        # pour Recomencer
+        #si oui
+            if b == "Y"
+                puts "> Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ? (entre 1 et 25) "
+                user_reload = gets.chomp.to_i
+                Getinput(user_reload)
+        #si non
+            elsif b == "N"
+                exit
+            #on verifie si entre 1 et 25 
+            elsif b <= 25 && b >= 1 
+            #si oui on exectute la creation de la pyramide
+                exec_pyra(b)       
+            #si non on redemande un nombre a l'utilisateur
+            else
+                    puts "un nombre entre 1 et 25 stp"
+                    Getinput(gets.chomp.to_i)
+            end
+    end
+
+    def exec_pyra(c)
+            c.times do |i|
+                ligne_pyra = "#" * (i *2 +1)
+                space = c - i
+                puts " " * space + ligne_pyra 
+            end
+            #fonction pour recomencer
+                recommencer()
+    end    
+
+
+    def recommencer()
+        #ici manque uniquement Y ou N comme entrer 
+        #d'autre entrer casserons le code 
+            puts "> voulez vous recomencer ?"
+            puts "> Taper Y ou N "
+            # on recupere oui ou non en maj
+            input = gets.chomp.to_s.upcase
+            condition(input)
+    end
+#on appelle la fonction Getinput pour demarer l'execution du code
+Getinput(user_number)
+
 
 =begin 
 outpout attendu :
